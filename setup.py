3f991dd73a9429f01cc387129b4b5d988ca62e87
alingse <alingse@foxmail.com>
2022-08-10 22:14:28 +0800

    add docs

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+import io
 from setuptools import setup
+
+
+with io.open('README.md', encoding='utf-8') as f:
+    readme = f.read()
 
 
 setup(
     name='emoji-chengyu',
     version='0.0.5',
     url='https://github.com/alingse/emoji-chengyu',
-    description='',
+    description=readme,
     author='alingse',
     author_email='alingse@foxmail.com',
     license='MIT',
